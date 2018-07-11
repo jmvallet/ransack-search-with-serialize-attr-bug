1a73242653ecf3eb14099d97171738794ebb09cc
jmvallet <jm.vallet@gmail.com>
2018-07-11 14:12:08 +0200

    Improve model test

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class PostTest < ActiveSupport::TestCase
 
   test "Ransack search on serialized attribute" do
-    assert_equal 1, Post.ransack(content_cont: "hello").result(distinct: true).count
+    assert_equal 1, Post.where("content like ?", "%world%").count
+    assert_equal 1, Post.ransack(content_cont: "world").result(distinct: true).count
   end
 
 end
